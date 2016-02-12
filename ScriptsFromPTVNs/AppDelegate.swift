05f08aeff664a0b9cfdd2277cd4ae01325b10f23
FoolOfShadows <foolofshadows@gmail.com>
2016-02-12 08:28:35 -0600

    Initial Commit

---
+++ ScriptsFromPTVNs/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ScriptsFromPTVNs
+//
+//  Created by Fool on 2/12/16.
+//  Copyright © 2016 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
