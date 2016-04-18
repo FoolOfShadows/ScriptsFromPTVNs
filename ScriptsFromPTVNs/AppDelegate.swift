4cf7ca83017cae0187965d870e8846ae9a2880ca
FoolOfShadows <foolofshadows@gmail.com>
2016-04-18 10:49:41 -0500

    Add code to allow lines to be marked with "^^" in order to be pulled out from where ever they are in the note instead of just being in the RX section.

--- ScriptsFromPTVNs/AppDelegate.swift
+++ ScriptsFromPTVNs/AppDelegate.swift
@@ -12,16 +12,154 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 	@IBOutlet weak var window: NSWindow!
+	@IBOutlet weak var resultsWindow: NSWindow!
+	@IBOutlet weak var directoryLabel: NSTextField!
+	@IBOutlet var resultsView: NSTextView!
+
+	
+	//Create an NSFileManager to use with the file enumeration we'll be doing later
+	let fileManager = NSFileManager.defaultManager()
 
 
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
+		//Create a path to the users home directory
+		let basePath = NSHomeDirectory()
+		//Addend the path to the WPCMSharedFiles folder for the office machines to create a default selection.
+		//This requires all the computers this code runs on to have a folder named WPCMSharedFiles
+		//in their home directory.
+		directoryLabel.stringValue = "\(basePath)/WPCMSharedFiles/zDoctor Review/06 Dummy Files"
+		//let newOrigin = NSPoint(x: -10.0, y: 100.0)
+		//resultsView.setFrameOrigin(newOrigin)
 	}
 
+	@IBAction func takeSetDirectory(sender: AnyObject) {
+		let panel = NSOpenPanel()
+		panel.canChooseDirectories = true
+		panel.canChooseFiles = false
+		
+		panel.beginSheetModalForWindow(self.window!, completionHandler: {(returnCode) -> Void in
+			if returnCode == NSModalResponseOK {
+				var message = panel.directoryURL!.path!
+				self.directoryLabel.stringValue = message
+			}
+		})
+	}
+	
+	func processTheDirectory() -> [NSURL] {
+		var theFileNames = [NSURL]()
+		let originFolderURL = NSURL(fileURLWithPath: directoryLabel.stringValue)
+		let enumeratorOptions: NSDirectoryEnumerationOptions = [.SkipsHiddenFiles, .SkipsPackageDescendants]
+		if let theEnumerator = fileManager.enumeratorAtURL(originFolderURL, includingPropertiesForKeys: nil, options: enumeratorOptions, errorHandler: nil) {
+			
+			for item in theEnumerator.allObjects {
+				if let itemURL = item as? NSURL {
+					if itemURL.absoluteString.containsString("PTVN") {
+						//if let theFileName = itemURL.URLByDeletingPathExtension {
+							theFileNames.append(itemURL)
+						//}
+					}
+				}
+			}
+		}
+		print(theFileNames)
+		return theFileNames
+	}
+	
+	func getNameInfo(theText:String) -> String {
+		var nameInfo = ""
+		
+		let separatedText = theText.componentsSeparatedByString("\n")
+		print(separatedText)
+		nameInfo = separatedText[0].stringByTrimmingLeadingAndTrailingWhitespace()
+		
+		return nameInfo
+	}
+	
+	func getDOBInfo(theText:String) -> String {
+		var dobInfo = ""
+		
+		let rawDOBInfo = regexTheText(theText, startOfText: "DOB:", endOfText: "Age:")
+		let badBits = ["DOB:", "Age:"]
+		dobInfo = cleanTheSections(rawDOBInfo, badBits: badBits)
+		dobInfo = dobInfo.stringByTrimmingLeadingAndTrailingWhitespace()
+		return dobInfo
+	}
+	
+	func getRxInfo(theText:String) -> String {
+		var rxInfo = ""
+		let rawNeededScripts = regexTheText(theText, startOfText: "\\*\\*Rx\\*\\*", endOfText: "O\\(PE\\):")
+		print("the rawNeededScripts is \(rawNeededScripts)")
+		let badBits = ["**Rx**", "O(PE):"]
+		rxInfo = cleanTheSections(rawNeededScripts, badBits: badBits)
+		rxInfo = rxInfo.stringByTrimmingLeadingAndTrailingWhitespace()
+		return rxInfo
+	}
+	
+	func getMarkedLines(theText:String) -> String {
+		var markedLines = [String]()
+		var results = String()
+		let theLines = theText.componentsSeparatedByString("\n")
+		for line in theLines {
+			if line.containsString("^^") {
+				let cleanLine = cleanTheSections(line, badBits: ["^^"])
+				markedLines.append(cleanLine)
+			}
+		}
+		results = markedLines.joinWithSeparator("\n")
+		return results
+	}
+	
+	func processTheFiles(theFiles:[NSURL]?) -> String {
+		var neededRxs = [""]
+		var results = ""
+		if let thePTVNText = theFiles {
+			for file in thePTVNText {
+					print("Starting the DO clause for \(file) in processTheFiles")
+					do {
+						let ptvnContents = try String(contentsOfURL: file, encoding: NSUTF8StringEncoding)
+						
+						let rxResults = getRxInfo(ptvnContents)
+						let dobResults = getDOBInfo(ptvnContents)
+						let nameResults = getNameInfo(ptvnContents)
+						let markedResults = getMarkedLines(ptvnContents)
+						if (rxResults != "") && (markedResults != "") {
+						neededRxs.append("\(nameResults) (DOB \(dobResults)) - \(rxResults)\n\(markedResults)")
+						} else if (rxResults != "") || (markedResults != "") {
+							neededRxs.append("\(nameResults) (DOB \(dobResults)) - \(rxResults)\(markedResults)")
+						}
+					} catch {
+						print("Ended up in the CATCH clause")
+					}
+				}
+			if neededRxs != [""] {
+				results = neededRxs.joinWithSeparator("\n\n")
+			}
+		}
+		
+			print(results)
+			return results
+		}
+	
+	@IBAction func takeFind(sender: AnyObject) {
+		let theFileURLs = processTheDirectory()
+		let theResults = processTheFiles(theFileURLs)
+		let theUserFont:NSFont = NSFont.systemFontOfSize(18)
+		let fontAttributes = NSDictionary(object: theUserFont, forKey: NSFontAttributeName)
+		resultsView.typingAttributes = fontAttributes as! [String : AnyObject]
+		resultsView.string = "NEEDED SCRIPTS\n\(theResults)"
+		resultsWindow.makeKeyAndOrderFront(self)
+	}
+	func takePrintAction() {
+		let printerInfo = NSPrintInfo()
+	}
+	
 	func applicationWillTerminate(aNotification: NSNotification) {
 		// Insert code here to tear down your application
 	}
-
+	
+func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+		return true
+	}
 
 }
 
---
+++ ScriptsFromPTVNs/Functions.swift
@@ -0,0 +1,172 @@
+//
+//  Functions.swift
+//  Chart Parsing
+//
+//  Created by Fool on 6/17/15.
+//  Copyright (c) 2015 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+
+//Cribbed from raywenderlich.com.  Updated for Swift 2.0
+extension String {
+	func stringByTrimmingLeadingAndTrailingWhitespace() -> String {
+		let leadingAndTrailingWhitespacePattern = "\\s*\\n"
+		//The original pattern, which didn't work the way I wanted was:
+		//"(?:^\\s+)|(?:\\s+$)"
+		
+		do {
+			let regex = try NSRegularExpression(pattern: leadingAndTrailingWhitespacePattern, options: .DotMatchesLineSeparators)
+			let range = NSMakeRange(0, self.characters.count)
+			let trimmedString = regex.stringByReplacingMatchesInString(self, options: .ReportProgress, range:range, withTemplate:"\n")
+			
+			return trimmedString
+		} catch _ {
+			return self
+		}
+	}
+}
+
+
+
+//Check for the existence of certain strings in the text
+//in order to determine the best string to use in the regexTheText function
+func defineFinalParameter(theText: String, firstParameter: String, secondParameter: String) -> String {
+	var theParameter = ""
+	if theText.rangeOfString(firstParameter) != nil {
+		theParameter = firstParameter
+	} else if theText.rangeOfString(secondParameter) != nil {
+		theParameter = secondParameter
+	}
+	return theParameter
+}
+
+//Extract the text for the different sections from the complete text
+func regexTheText(theText: String, startOfText: String, endOfText: String) -> String {
+	var theResult = ""
+	let regex = try! NSRegularExpression(pattern: "\(startOfText).*?\(endOfText)", options: NSRegularExpressionOptions.DotMatchesLineSeparators)
+	let length = theText.characters.count
+	
+	if let match = regex.firstMatchInString(theText, options: [], range: NSRange(location: 0, length: length)) {
+		theResult = (theText as NSString).substringWithRange(match.range)
+	}
+	return theResult
+}
+	
+//Clean extraneous text from the sections
+func cleanTheSections(theSection:String, badBits:[String]) -> String {
+	var cleanedText = theSection.stringByTrimmingLeadingAndTrailingWhitespace()
+	for theBit in badBits {
+		cleanedText = cleanedText.stringByReplacingOccurrencesOfString(theBit, withString: "")
+	}
+	cleanedText = cleanedText.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+	return cleanedText
+}
+
+//A basic regular expression search function
+func simpleRegExMatch(theText: String, theExpression: String) -> String {
+	var theResult = ""
+	let regEx = try! NSRegularExpression(pattern: theExpression, options: [])
+	let length = theText.characters.count
+	
+	if let match = regEx.firstMatchInString(theText, options: [], range: NSRange(location: 0, length: length)) {
+		theResult = (theText as NSString).substringWithRange(match.range)
+	}
+	return theResult
+}
+
+//Adjust visit date values based on how far the visit is scheduled into the future
+func addingDays (theDate: NSDate, daysToAdd: Int) -> NSDate {
+	let components:NSDateComponents = NSDateComponents()
+	components.setValue(daysToAdd, forComponent: NSCalendarUnit.Day);
+	let newDate = NSCalendar.currentCalendar().dateByAddingComponents(components, toDate: theDate, options: NSCalendarOptions(rawValue:0))
+	return newDate!
+}
+
+//Add specific characters to the beginning of each line
+func addCharatersToFront(theText:String, theCharacters:String) ->String {
+	var returnText = ""
+	var newTextArray = [String]()
+	let textArray = theText.componentsSeparatedByString("\n")
+	for line in textArray {
+		let newLine = "- " + line
+		newTextArray.append(newLine)
+	}
+	
+	returnText = newTextArray.joinWithSeparator("\n")
+	
+	return returnText
+}
+
+//Parse a string containing a full name into it's components and returns
+//the version of the name we use to label files
+func getFileLabellingName(name: String) -> String {
+	var fileLabellingName = String()
+	var ptFirstName = ""
+	var ptLastName = ""
+	var ptMiddleName = ""
+	var ptExtraName = ""
+	let extraNameBits = ["Sr", "Jr", "II", "III", "IV", "MD"]
+	
+	func checkForMatchInSets(arrayToCheckIn: [String], arrayToCheckFor: [String]) -> Bool {
+		var result = false
+		for item in arrayToCheckIn {
+			if arrayToCheckFor.contains(item) {
+				result = true
+				break
+			}
+		}
+		return result
+	}
+	
+	let nameComponents = name.componentsSeparatedByString(" ")
+	
+	let extraBitsCheck = checkForMatchInSets(nameComponents, arrayToCheckFor: extraNameBits)
+	
+	if extraBitsCheck == true {
+		ptLastName = nameComponents[nameComponents.count-2]
+		ptExtraName = nameComponents[nameComponents.count-1]
+	} else {
+		ptLastName = nameComponents[nameComponents.count-1]
+		ptExtraName = ""
+	}
+	
+	if nameComponents.count > 2 {
+	if nameComponents[nameComponents.count - 2] == "Van" {
+		ptLastName = "Van " + ptLastName
+	}
+	}
+	
+	//Get first name
+	ptFirstName = nameComponents[0]
+	
+	//Get middle name
+	if (nameComponents.count == 3 && extraBitsCheck == true) || nameComponents.count < 3 {
+		ptMiddleName = ""
+	} else {
+		ptMiddleName = nameComponents[1]
+	}
+	
+	fileLabellingName = "\(ptLastName)\(ptFirstName)\(ptMiddleName)\(ptExtraName)"
+	fileLabellingName = fileLabellingName.stringByReplacingOccurrencesOfString(" ", withString: "")
+	fileLabellingName = fileLabellingName.stringByReplacingOccurrencesOfString("-", withString: "")
+	fileLabellingName = fileLabellingName.stringByReplacingOccurrencesOfString("'", withString: "")
+	fileLabellingName = fileLabellingName.stringByReplacingOccurrencesOfString("(", withString: "")
+	fileLabellingName = fileLabellingName.stringByReplacingOccurrencesOfString(")", withString: "")
+	fileLabellingName = fileLabellingName.stringByReplacingOccurrencesOfString("\"", withString: "")
+	
+	
+return fileLabellingName
+}
+	
+	//A way to extract text using mapping and filtering.  I've changed to using regular expressions
+	//func extractTheText(theText: String, startOfText: String, endOfText: String) -> [String] {
+	//		let theArray = theText.componentsSeparatedByString(startOfText)
+	//			.map { $0.componentsSeparatedByString(endOfText) }
+	//			.filter { $0.count > 1 }
+	//			.map { $0[0] }
+	//		return theArray
+	//
+	//}
+	
