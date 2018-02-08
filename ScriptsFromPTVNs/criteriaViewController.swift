0458ddee5fffd535eb27de51ee05cc8890102bbd
FoolOfShadows <foolofshadows@gmail.com>
2018-02-08 11:36:16 -0600

    Created new VC stubs, getting ready to branch to refactor

---
+++ ScriptsFromPTVNs/criteriaViewController.swift
@@ -0,0 +1,18 @@
+//
+//  criteriaViewController.swift
+//  ScriptsFromPTVNs
+//
+//  Created by Fool on 2/8/18.
+//  Copyright © 2018 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+class criteriaViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ ScriptsFromPTVNs/resultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  resultsViewController.swift
+//  ScriptsFromPTVNs
+//
+//  Created by Fool on 2/8/18.
+//  Copyright © 2018 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+class resultsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
